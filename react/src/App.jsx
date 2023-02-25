bbebd8ba1c3b6b19761bcbec775fd775a3c108a6
ristic21 <aleksandar.ristic21498@gmail.com>
2023-02-25 19:39:21 +0100

    initial commit-app-setup

---
+++ react/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import { BeakerIcon } from '@heroicons/react/24/solid'
+import './App.css'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="bg-purple-500">
+      App.jsx
+      <BeakerIcon className='w-4' />
+    </div>
+  )
+}
+
+export default App
